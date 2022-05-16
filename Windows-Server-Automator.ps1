dd84d0ad2b866b07984574ce3d20770ff176a168
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-05-16 14:17:23 +0200

    v0.2 submittet
    added: Optimization

--- Windows-Server-Automator.ps1
+++ Windows-Server-Automator.ps1
@@ -1,61 +1,68 @@
 ﻿# Prepare
-    $path_install = "C:\Program Files\WindowsPowerShell\Modules\WinSerAuto"
-        New-Item -ItemType Directory $path_install -ErrorAction SilentlyContinue | Out-Null
-    
-    $modules = "WinSerAuto_HostConfigurator","WinSerAuto_RoleConfigurator","WinSerAuto_ADConfigurator","WinSerAuto_ShareConfigurator"
-        $modules | % {iwr -useb "https://raw.githubusercontent.com/Andreas6920/WinSerAuto/main/res/modules/$_.psm1" -OutFile $path_install\$_.psm1}
+$path_install = "C:\Program Files\WindowsPowerShell\Modules\Windows-Server-Automator"
+    New-Item -ItemType Directory $path_install -ErrorAction SilentlyContinue | Out-Null
 
-    $reg_install = "HKLM:\Software\WinSerAuto"
-        If (!(Test-Path $reg_install)) {New-Item -Path $reg_install -Force | Out-Null; $modules | % {Set-ItemProperty -Path $reg_install -Name $_ -Type DWord -Value 0 | Out-Null}}
+$modules = "WinSerAuto-HostConfigurator","WinSerAuto-RoleConfigurator","WinSerAuto-ADConfigurator","WinSerAuto-ShareConfigurator"
+    $modules | % {iwr -useb "https://raw.githubusercontent.com/Andreas6920/Windows-Server-Automator/main/res/modules/$_.psm1" -OutFile $path_install\$_.psm1; Start-Sleep -s 1}
+
+$reg_install = "HKLM:\Software\WinSerAuto"
+    If (!(Test-Path $reg_install)) {New-Item -Path $reg_install -Force | Out-Null; $modules | % {Set-ItemProperty -Path $reg_install -Name $_ -Type DWord -Value 0 | Out-Null}}
 
 
 # Menu
-
-    $logo = 
+Clear-Host
+$logo = 
 "
-__        ___           _                     ____                           
-\ \      / (_)_ __   __| | _____      _____  / ___|  ___ _ ____   _____ _ __ 
- \ \ /\ / /| | '_ \ / _` |/ _ \ \ /\ / / __| \___ \ / _ \ '__\ \ / / _ \ '__|
-  \ V  V / | | | | | (_| | (_) \ V  V /\__ \  ___) |  __/ |   \ V /  __/ |   
-   \_/\_/  |_|_|_|_|\__,_|\___/ \_/\_/ |___/ |____/_\___|_|    \_/ \___|_|   
-               / \  _   _| |_ ___  _ __ ___   __ _| |_ ___  _ __             
-              / _ \| | | | __/ _ \| '_ ` _ \ / _` | __/ _ \| '__|            
-             / ___ \ |_| | || (_) | | | | | | (_| | || (_) | |               
-            /_/   \_\__,_|\__\___/|_| |_| |_|\__,_|\__\___/|_|               
-                                                                             
-Version 0.1
+
+██╗    ██╗██╗███╗   ██╗██████╗  ██████╗ ██╗    ██╗███████╗    ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ 
+██║    ██║██║████╗  ██║██╔══██╗██╔═══██╗██║    ██║██╔════╝    ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+██║ █╗ ██║██║██╔██╗ ██║██║  ██║██║   ██║██║ █╗ ██║███████╗    ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
+██║███╗██║██║██║╚██╗██║██║  ██║██║   ██║██║███╗██║╚════██║    ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
+╚███╔███╔╝██║██║ ╚████║██████╔╝╚██████╔╝╚███╔███╔╝███████║    ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
+ ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚══════╝    ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+                                                                                                               
+                 █████╗ ██╗   ██╗████████╗ ██████╗ ███╗   ███╗ █████╗ ████████╗ ██████╗ ██████╗                
+                ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗               
+                ███████║██║   ██║   ██║   ██║   ██║██╔████╔██║███████║   ██║   ██║   ██║██████╔╝               
+                ██╔══██║██║   ██║   ██║   ██║   ██║██║╚██╔╝██║██╔══██║   ██║   ██║   ██║██╔══██╗               
+                ██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║ ╚═╝ ██║██║  ██║   ██║   ╚██████╔╝██║  ██║               
+                ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝               
+                                                                     
+Version 0.2
 Creator: Andreas6920 | https://github.com/Andreas6920/
+
+
 "                                                                   
 
-        # Change color if module already executed
-        if ((Get-ItemProperty -Path "HKLM:\Software\WinSerAuto" -Name "WinSerAuto_HostConfigurator").WinSerAuto_HostConfigurator -eq 0){$menu1 = "Green"} else {$menu1 = "DarkGray"}
-        if ((Get-ItemProperty -Path "HKLM:\Software\WinSerAuto" -Name "WinSerAuto_RoleConfigurator").WinSerAuto_RoleConfigurator -eq 0){$menu2 = "Green"} else {$menu2 = "DarkGray"}
-        if ((Get-ItemProperty -Path "HKLM:\Software\WinSerAuto" -Name "WinSerAuto_ADConfigurator").WinSerAuto_ADConfigurator -eq 0){$menu3 = "Green"} else {$menu3 = "DarkGray"}
-        if ((Get-ItemProperty -Path "HKLM:\Software\WinSerAuto" -Name "WinSerAuto_ShareConfigurator").WinSerAuto_ShareConfigurator -eq 0){$menu4 = "Green"} else {$menu4 = "DarkGray"}
- 
-        # Option list
-        Clear-Host
-        "";
-        Write-host $logo -f Green
-        "";
-        Write-host "`t`t1) Host Configurator`t`t( Hostname / IP )" -f $menu1
-        Write-host "`t`t2) Baserole Configurator`t( Server Role / Domain Name )" -f $menu2
-        Write-host "`t`t3) AD Configurator`t`t`t( OU creation / User Creation / CSV import )" -f $menu3
-        Write-host "`t`t4) Share Configurator`t`t( Security Group Creator / NTFS / SMB )" -f $menu4
-        Write-host "`t`t0) Exit" -f Green
-        "";"";
+# Change color if module already executed
+if ((Get-ItemProperty -Path $reg_install -Name "WinSerAuto_HostConfigurator").WinSerAuto_HostConfigurator -eq 0){$menu1 = "Green"} else {$menu1 = "DarkGray"}
+if ((Get-ItemProperty -Path $reg_install -Name "WinSerAuto_RoleConfigurator").WinSerAuto_RoleConfigurator -eq 0){$menu2 = "Green"} else {$menu2 = "DarkGray"}
+if ((Get-ItemProperty -Path $reg_install -Name "WinSerAuto_ADConfigurator").WinSerAuto_ADConfigurator -eq 0){$menu3 = "Green"} else {$menu3 = "DarkGray"}
+if ((Get-ItemProperty -Path $reg_install -Name "WinSerAuto_ShareConfigurator").WinSerAuto_ShareConfigurator -eq 0){$menu4 = "Green"} else {$menu4 = "DarkGray"}
 
-        # User input
-        do {
-            Write-Host "Choose your option:`t" -f Green -nonewline; ; ;
-            $selection = Read-Host
-            Switch ($selection) {
+# Option list
+Clear-Host
+"";
+Write-host $logo -f Green
+"";
+Write-host "`t`t1) Host Configurator`t`t( Server Optimizer / Hostname / IP )" -f $menu1
+Write-host "`t`t2) Baserole Configurator`t( Server Role / Domain Name )" -f $menu2
+Write-host "`t`t3) AD Configurator`t`t`t( OU creation / User Creation / CSV import )" -f $menu3
+Write-host "`t`t4) Share Configurator`t`t( Security Group Creator / NTFS / SMB )" -f $menu4
+Write-host "`t`t0) Exit" -f Green
+"";"";
 
-                1 {    Import-Module "C:\Program Files\WindowsPowerShell\Modules\WinSerAuto\WinSerAuto_HostConfigurator"    }
-                2 {    Import-Module "C:\Program Files\WindowsPowerShell\Modules\WinSerAuto\WinSerAuto_RoleConfigurator"    }
-                3 {    Import-Module "C:\Program Files\WindowsPowerShell\Modules\WinSerAuto\WinSerAuto_ADConfigurator"      }
-                4 {    Import-Module "C:\Program Files\WindowsPowerShell\Modules\WinSerAuto\WinSerAuto_ShareConfigurator"   }
-                0 {                                         EXIT                                                        }
-           
-            }}
-        while ($selection -ne 0 )
+# User input
+do {
+    Write-Host "Choose your option:`t" -f Green -nonewline; ; ;
+    $selection = Read-Host
+    Switch ($selection) {
+
+        1 {    Import-Module "$path_install\WinSerAuto_HostConfigurator"    }
+        2 {    Import-Module "$path_install\WinSerAuto_RoleConfigurator"    }
+        3 {    Import-Module "$path_install\WinSerAuto_ADConfigurator"      }
+        4 {    Import-Module "$path_install\WinSerAuto_ShareConfigurator"   }
+        0 {                                         EXIT                                                        }
+   
+    }}
+while ($selection -ne 0 )
--- res/modules/WinSerAuto-HostConfigurator.psm1
+++ res/modules/WinSerAuto-HostConfigurator.psm1
@@ -1,91 +1,116 @@
-# PART 1 - Initial configurations
 
-    ## PART 1.1 - COMPUTERNAME
-    Write-host "PART 1.1 - PC NAME" -f Green
-    Do {
-        Write-Host "`tCurrent PC Name:" -f yellow -nonewline; Write-Host " $env:computername" -f Yellow
-        Write-Host "`tWould you like to rename this PC? (y/n)" -nonewline -f Yellow;
+    # PART 1 - Optimization
+        Write-host "PART 1 - Optimize Windows" -f Green
+        
+        Write-host "`t`t- Installing basic features in the background" -f Yellow
+            Start-Job -Name "Install Features" -ScriptBlock {Install-WindowsFeature "BitLocker","Direct-Play","Wireless-Networking","qWave"}
+
+        Write-host "`t`t- Disable Server Manager to pop-up when booting." -f Yellow
+            Set-ItemProperty -Path "HKLM:\Software\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -Value 1    
+        
+        Write-host "`t`t- Disable Microsoft Logging Tasks in scheduled tasks." -f Yellow
+            Get-ScheduledTask -taskpath "\Microsoft\Windows\Application Experience\*" | Stop-ScheduledTask | Out-Null
+            Get-ScheduledTask -taskpath "\Microsoft\Windows\Application Experience\*" | Disable-ScheduledTask | Out-Null
+            
+        Write-host "`t`t- Disable IE Security." -f Yellow
+            $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+            $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+            Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+            Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+            Stop-Process -Name Explorer
+
+        Write-host "`t`t- Disable 'Ctrl+Alt+Del to login' requirement" -f Yellow
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value 1
+
+        Write-host "`t`t- Disable shutdown reason requirement" -f Yellow
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" -Name "ShutdownReasonOn" -Value 0
+
+
+    # PART 2 - Computername
+        Write-host "PART 2- PC NAME" -f Green
+            Do {
+                Write-Host "`tCurrent PC Name:" -f yellow -nonewline; Write-Host " $env:computername" -f Yellow
+                Write-Host "`tWould you like to rename this PC? (y/n)" -nonewline -f Yellow;
+                $answer = Read-Host " " 
+                Switch ($answer) { 
+                    Y {
+                        $WarningPreference = "SilentlyContinue"
+                        Write-Host "`t`tNew PC name" -nonewline;
+                        $PCname = Read-Host " "
+                        Rename-computer -newname $PCname
+                        $WarningPreference = "Continue"
+                        $reboot = $true
+                        Write-Host "`t`tComputer description" -NoNewline
+                        $Description = Read-Host " " 
+                        $ThisPCDescription=Get-WmiObject -class Win32_OperatingSystem
+                        $ThisPCDescription.Description=$Description
+                        $ThisPCDescription.put() | out-null
+                        Write-Host "`t`tComputer renamed. PC will reboot after IP configuration." -f yellow; Sleep -s 2
+                        "";}
+                    N {Write-Host "`t`tNO - This PC will not be renamed." -f red -nonewline; $reboot = $false} 
+                    }   
+                } While ($answer -notin "y", "n")    
+
+    # PART 3 - IP configuration
+        Write-host "PART 3 - IP CONFIGURATION" -f Green; Sleep -s 2
+        Do {
+        Write-Host "`tYour Network Adapters:" -f yellow
+        # Get network adapters
+        $nic = Get-NetIPAddress -AddressFamily IPv4;"";
+        foreach ($n in $nic){ $int = $n.InterfaceAlias;write-host "`t`t$int ( IP:"$n.IPAddress")" -f Yellow}; Start-Sleep -s 2;"";
+
+        Write-Host "`tWould you like to change your IP? (y/n)" -nonewline -f Yellow;
         $answer = Read-Host " " 
         Switch ($answer) { 
-            Y {
-                
-                $WarningPreference = "SilentlyContinue"
-                Write-Host "`t`tNew PC name" -nonewline;
-                $PCname = Read-Host " "
-                Rename-computer -newname $PCname
-                $WarningPreference = "Continue"
-                $reboot = $true
-                Write-Host "`t`tComputer description" -NoNewline
-                $Description = Read-Host " " 
-                $ThisPCDescription=Get-WmiObject -class Win32_OperatingSystem
-                $ThisPCDescription.Description=$Description
-                $ThisPCDescription.put() | out-null
-                Write-Host "`t`tComputer renamed. PC will reboot after IP configuration." -f yellow; Sleep -s 2
-                "";}
-            N {Write-Host "`t`tNO - This PC will not be renamed." -f red -nonewline; $reboot = $false} 
-            }   
-        } While ($answer -notin "y", "n")    
 
-## PART 1.2 - IP CONFIGURATION
-Write-host "PART 1.2 - IP CONFIGURATION" -f Green; Sleep -s 2
-Do {
-Write-Host "`tYour Network Adapters:" -f yellow
-# Get network adapters
-$nic = Get-NetIPAddress -AddressFamily IPv4;"";
-foreach ($n in $nic){ $int = $n.InterfaceAlias;write-host "`t`t$int ( IP:"$n.IPAddress")" -f Yellow}; Start-Sleep -s 2;"";
-
-Write-Host "`tWould you like to change your IP? (y/n)" -nonewline -f Yellow;
-$answer = Read-Host " " 
-Switch ($answer) { 
-
-    Y {
-    
-        Do {Write-Host "`t`t`tPlease enter the NAME of the primary network interface card" -nonewline;
-            $ethernetadaptername = Read-Host " " } 
-
-        While ($ethernetadaptername -notin ((Get-NetIPAddress -AddressFamily IPv4).InterfaceAlias)) 
-
-        # Get network settings
-            $currentip = netsh interface ip show addresses $ethernetadaptername | select-string "IP Address"
-            $currentsubnet = "/"+(Get-NetIPAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4).PrefixLength
-            $currentgateway = (netsh interface ip show addresses $ethernetadaptername | select-string "Default gateway")[0]
-            $currentDNS = (Get-DnsClientServerAddress -InterfaceAlias Ethernet0).ServerAddresses
-            "";
-            Write-Host "`t`t`tCurrent IP Settings:" -f yellow
-            Write-Host "`t`t`tInterface Name:`t`t`t`t`t`t  $ethernetadaptername" -f Yellow
-            Write-Host "`t`t$currentip" -f Yello
-            Write-Host "`t`t`tSubnet:`t`t`t`t`t`t`t`t "$currentsubnet -f Yellow
-            Write-Host "`t`t$currentgateway" -f Yellow;""; 
+        Y {
         
+            Do {Write-Host "`t`t`tPlease enter the NAME of the primary network interface card" -nonewline;
+                $ethernetadaptername = Read-Host " " } 
 
-        # Enter New adapter settings
-        Do {Write-Host "`t`t`tEnter new IP Address" -f yellow -NoNewline
-        $newIP = Read-Host " " } While ($newIP -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-        Do {Write-Host "`t`t`tEnter new SUBNET VALUE (example: 24, 16, 8)" -f yellow -NoNewline
-        $newSubnet = Read-Host " "} While ($newSubnet -notin 8..30)
-        Do {Write-Host "`t`t`tEnter new gateway IP:" -f yellow -NoNewline
-        $newGW = Read-Host " "} While ($newGW -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-        Do {Write-Host "`t`t`tEnter new DNS IP:" -f yellow -NoNewline
-        $newDNS = Read-Host " "} While ($newDNS -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+            While ($ethernetadaptername -notin ((Get-NetIPAddress -AddressFamily IPv4).InterfaceAlias)) 
 
-        # Configure new settings
-        "";
-        Write-Host "`t`t`tNEW CONFIGURATION IS BEING SET:" -f yellow;
-        Write-Host "`t`t`t - Clearing current settings.." -f Yellow;
-        Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Parameters\Interfaces\$((Get-NetAdapter -InterfaceAlias $ethernetadaptername).InterfaceGuid) -Name EnableDHCP -Value 0 -ea SilentlyContinue
-        Remove-NetIpAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
-        Remove-NetRoute -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
-        Start-Sleep -s 2
+            # Get network settings
+                $currentip = netsh interface ip show addresses $ethernetadaptername | select-string "IP Address"
+                $currentsubnet = "/"+(Get-NetIPAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4).PrefixLength
+                $currentgateway = (netsh interface ip show addresses $ethernetadaptername | select-string "Default gateway")[0]
+                $currentDNS = (Get-DnsClientServerAddress -InterfaceAlias Ethernet0).ServerAddresses
+                "";
+                Write-Host "`t`t`tCurrent IP Settings:" -f yellow
+                Write-Host "`t`t`tInterface Name:`t`t`t`t`t`t  $ethernetadaptername" -f Yellow
+                Write-Host "`t`t$currentip" -f Yello
+                Write-Host "`t`t`tSubnet:`t`t`t`t`t`t`t`t "$currentsubnet -f Yellow
+                Write-Host "`t`t$currentgateway" -f Yellow;""; 
+            
 
-        Write-Host "`t`t`t - Setting new IP..." -f Yellow; Start-Sleep -s 1
-        Write-Host "`t`t`t - Setting new Subnet..." -f Yellow; Start-Sleep -s 1
-        Write-Host "`t`t`t - Setting new Gateway..." -f Yellow; Start-Sleep -s 1
-        New-NetIpAddress -InterfaceAlias $ethernetadaptername -IpAddress $newIP -PrefixLength $newSubnet -DefaultGateway $newGW -AddressFamily IPv4 | out-null
-        Write-Host "`t`t`t - Setting new DNS..." -f Yellow; Start-Sleep -s 1
-        Set-DnsClientServerAddress -InterfaceAlias $ethernetadaptername -ServerAddresses $newDNS | out-null
-        Write-Host "`t`t`tIP SETTING COMPLETE!" -f yellow; Start-Sleep -S 1
-        }
-    N  {    Write-Host "`t`tNo, this step will be skipped." -f red; Start-Sleep -s 2;   }
+            # Enter New adapter settings
+            Do {Write-Host "`t`t`tEnter new IP Address" -f yellow -NoNewline
+            $newIP = Read-Host " " } While ($newIP -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+            Do {Write-Host "`t`t`tEnter new SUBNET VALUE (example: 24, 16, 8)" -f yellow -NoNewline
+            $newSubnet = Read-Host " "} While ($newSubnet -notin 8..30)
+            Do {Write-Host "`t`t`tEnter new gateway IP:" -f yellow -NoNewline
+            $newGW = Read-Host " "} While ($newGW -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+            Do {Write-Host "`t`t`tEnter new DNS IP:" -f yellow -NoNewline
+            $newDNS = Read-Host " "} While ($newDNS -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+
+            # Configure new settings
+            "";
+            Write-Host "`t`t`tNEW CONFIGURATION IS BEING SET:" -f yellow;
+            Write-Host "`t`t`t - Clearing current settings.." -f Yellow;
+            Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Parameters\Interfaces\$((Get-NetAdapter -InterfaceAlias $ethernetadaptername).InterfaceGuid) -Name EnableDHCP -Value 0 -ea SilentlyContinue
+            Remove-NetIpAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+            Remove-NetRoute -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+            Start-Sleep -s 2
+
+            Write-Host "`t`t`t - Setting new IP..." -f Yellow; Start-Sleep -s 1
+            Write-Host "`t`t`t - Setting new Subnet..." -f Yellow; Start-Sleep -s 1
+            Write-Host "`t`t`t - Setting new Gateway..." -f Yellow; Start-Sleep -s 1
+            New-NetIpAddress -InterfaceAlias $ethernetadaptername -IpAddress $newIP -PrefixLength $newSubnet -DefaultGateway $newGW -AddressFamily IPv4 | out-null
+            Write-Host "`t`t`t - Setting new DNS..." -f Yellow; Start-Sleep -s 1
+            Set-DnsClientServerAddress -InterfaceAlias $ethernetadaptername -ServerAddresses $newDNS | out-null
+            Write-Host "`t`t`tIP SETTING COMPLETE!" -f yellow; Start-Sleep -S 1}
+            
+        N  {    Write-Host "`t`tNo, this step will be skipped." -f red; Start-Sleep -s 2;   }
 
 } }While ($answer -notin "y", "n")
 
