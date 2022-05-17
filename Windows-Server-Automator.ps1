c5de1c0d8a6d009e79a902240975c9c70d79ac10
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-05-17 15:40:41 +0200

    Update Windows-Server-Automator.ps1

--- Windows-Server-Automator.ps1
+++ Windows-Server-Automator.ps1
@@ -5,8 +5,8 @@ $modules = "WinSerAuto-HostConfigurator","WinSerAuto-RoleConfigurator","WinSerAu
 $reg_install = "HKLM:\Software\WinSerAuto"
 
 if((Get-ScheduledTask -TaskName "Windows-Server-Automator")){
-Get-ScheduledTask -TaskName "Windows-Server-Automator" | Stop-ScheduledTask | out-null
-Get-ScheduledTask -TaskName "Windows-Server-Automator" | Disable-ScheduledTask | out-null
+Get-ScheduledTask -TaskName "Windows-Server-Automator" -ErrorAction SilentlyContinue | Stop-ScheduledTask | out-null
+Get-ScheduledTask -TaskName "Windows-Server-Automator" -ErrorAction SilentlyContinue | Disable-ScheduledTask | out-null
 }
     Start-Job -Name "Preparing in background" -ScriptBlock {
         $path_install = "C:\Program Files\WindowsPowerShell\Modules\Windows-Server-Automator"
